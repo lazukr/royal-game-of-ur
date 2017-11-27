3a7564f53adf7b4afc08df7687854116fa87437a
lazukr <peterh10201@gmail.com>
2017-11-27 03:56:33 +0100

    rewriting the whole game

---
+++ game.py
@@ -0,0 +1,24 @@
+import pygame
+import math
+import random
+
+# colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+YELLOW = (255, 255, 0)
+BG_ORANGE = (198, 106, 17)
+
+
+# coordinate constants
+GRID = 45
+SQUARE = (GRID, GRID)
+BOARD = (8, 3)
+BTN_SIZE = (135, 35)
+WINDOW_HEIGHT = 11*GRID
+WINDOW_WIDTH = 12*GRID
+BOARD_OFFSET = (5, 4*GRID)
+
+
